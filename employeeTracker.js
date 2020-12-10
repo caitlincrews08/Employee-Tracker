b36200a5c7a6399a3750141cc590b450d213cfff
Caitlin Crews <caitlinrcrews@gmail.com>
2020-12-10 01:16:51 -0500

    created endConnection function and addDept function

--- employeeTracker.js
+++ employeeTracker.js
@@ -12,11 +12,80 @@ var connection = mysql.createConnection({
   user: "root",
 
   // Your password
-  password: "1234",
+  password: "Ghost08.",
   database: "employeetracker_db"
 });
 
+// establishes connection to server
 connection.connect(function(err) {
   if (err) throw err;
   console.log("connected as id " + connection.threadId + "\n");
+  init();
 });
+
+// prompts user with questions from command line then routes user to proper function below
+function init() {
+  inquirer.prompt({
+    name: "start",
+    type: "list",
+    message: "What would you like to do?",
+    choices: [
+      "Add a department",
+      "Add a role",
+      "Add an employee",
+      "View departments",
+      "View roles",
+      "View employees",
+      "Update employee roles",
+      "Nothing"
+    ]
+  }).then(function(res) {
+    switch (res.action) {
+      case "Add a department":
+        addDept();
+        break;
+
+      case "Add a role":
+        addRole();
+        break;
+
+      case "Add an employee":
+        addEmp();
+        break;
+        
+      case "View departments":
+        viewDept();
+        break;
+
+      case "View roles":
+        viewRoles();
+        break;
+
+      case "View employees":
+        viewEmps();
+        break;
+
+      case "Update employee roles":
+        updateRole();
+        break;
+
+      case "Nothing":
+        endConnection();
+        break;
+    }
+  });
+}
+
+// why isnt this working??
+function endConnection() {
+  console.log("thanks for using employee tracker!")
+  connection.end();
+}
+
+function addDept() {
+  inquirer.prompt({
+    name: "newDept",
+    type: "input",
+    message: "Please enter a name for the new department"
+  });
+}
